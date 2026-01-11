bb80952813fc60c27f919811a6885c79d791155e
AbuCoder <abubakrabdujalilov09@gmail.com>
2026-01-11 21:32:43 +0300

    Auth folders added4

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on http://localhost:3000");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
