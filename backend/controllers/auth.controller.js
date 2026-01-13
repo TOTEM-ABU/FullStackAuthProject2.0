bd6600028f790c87f5d79bfde5ae6e7f34bcc591
AbuCoder <abubakrabdujalilov09@gmail.com>
2026-01-13 22:45:52 +0300

    Signup method completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,47 @@
+import bcryptjs from "bcryptjs";
+
+import User from "../models/auth.model.js";
+import { generateVerificationCode } from "../utils/generateVerificationCode.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 export const signup = async (req, res) => {
-  res.send("Signup Route");
+  const { email, password, username } = req.body;
+
+  try {
+    if (!email || !password || !username) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists." });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = generateVerificationCode();
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      username,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+    res.status(201).json({
+      success: true,
+      message: "User created successfully.",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error.", error: error.message });
+  }
 };
 
 export const login = async (req, res) => {
