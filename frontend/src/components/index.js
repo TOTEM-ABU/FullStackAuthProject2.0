590c5be2ce2b1c7da254fd66c13133c04c6b224b
AbuCoder <abubakrabdujalilov09@gmail.com>
2026-01-29 00:33:39 +0300

    Signup and Login pages UI done

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,4 @@
 export { default as TubesScene } from "./TubesScene.jsx";
+export { default as FloatingShape } from "./FloatingShape.jsx";
+export { default as Input } from "./Input.jsx";
+export { default as PasswordStrengthMeter } from "./PasswordStrengthMeter.jsx";
